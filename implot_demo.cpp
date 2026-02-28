4b06523f89830d9fc84168d02c4a2e6f12450042
Brenton Bostick <bostick@gmail.com>
2026-02-28 08:59:00 -0500

    ImPlot fixes: extra ;

--- implot_demo.cpp
+++ implot_demo.cpp
@@ -1416,7 +1416,7 @@ void Demo_AutoFittingData() {
         ImPlot::PlotLine("Line",data,101);
         ImPlot::PlotStems("Stems",data,101);
         ImPlot::EndPlot();
-    };
+    }
 }
 
 //-----------------------------------------------------------------------------
